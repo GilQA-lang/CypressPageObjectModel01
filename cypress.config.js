9016564ec361095e155239d65ce021362c1f48e1
GilkeiraQA <gilmared@gmail.com>
2024-11-25 22:53:07 -0500

    Cambios Final2

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     reporter: 'cypress-mochawesome-reporter',
-   /* reporterOptions:{
-    overwrite: true,
-    reportFilename: 'reporte-tes',
-    timestamp: 'shorDate',
-  }*/
+    reporterOptions:{
+      overwrite: true,
+      reportFilename: 'reporte-tes',
+      //timestamp: 'shorDate',//Agrega fecha
+      inlineAssets:true //Independiza el reporte HTML
+
+  },
   
+
     e2e: {
     screenshotOnRunFailure:true,
     screenshotsFolder:"cypress/screenshosts,",
